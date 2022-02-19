49ad591a7032218764b51f3cf9dd56e9482d2331
tangkunjiu <756845786@qq.com>
2022-02-19 14:11:46 +0800

    第二次提交gits

--- src/main/java/com/ryitedu/application/Test.java
+++ src/main/java/com/ryitedu/application/Test.java
@@ -3,5 +3,6 @@ package com.ryitedu.application;
 public class Test {
     public static void main(String[] args) {
         System.out.println("nihao");
+        System.out.println("123455");
     }
 }
