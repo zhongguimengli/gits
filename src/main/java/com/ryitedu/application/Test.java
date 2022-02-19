ed8f659100d2fa1c9ec6d19397b9ef547d1cda6b
tangkunjiu <756845786@qq.com>
2022-02-19 15:03:39 +0800

    hot-git 第一次提交 加了个for循环

--- src/main/java/com/ryitedu/application/Test.java
+++ src/main/java/com/ryitedu/application/Test.java
@@ -4,5 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("123455");
+        for (int a = 0; a < 100; a++){
+            System.out.println(a);
+        }
     }
 }
